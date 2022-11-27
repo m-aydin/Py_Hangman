aaed8c95c525bdba0b0f99bc563f45e5ee0a897f
m-aydin <115656778+m-aydin@users.noreply.github.com>
2022-11-27 20:20:57 +0000

    Updated py_hangman.py

--- src/py_hangman.py
+++ src/py_hangman.py
@@ -12,39 +12,50 @@ def init_game(url):
     to_display="#"*len(to_guess)
     
 #Constants
-PICK_LIMIT=26
+PICK_LIMIT=6
+IS_CHEAT=False
 
 #Global Variables
 to_guess=""
 all_picks=[]
 current_pick=""
-wrong_picks=0
+wrong_picks=[0,""]
 
 #Main Program
 init_game("https://www.mit.edu/~ecprice/wordlist.10000")
 
 while True:
-    print("\nWord So Far: "+to_display)
-    current_pick=input("Input Letter,"+str(PICK_LIMIT-wrong_picks)+" Picks Left: ").lower()[0]
+    wrong_picks[1]=""
+
+    for current in all_picks:
+        if current in to_guess:
+            continue
+        else:
+            wrong_picks[1]+=","+current.upper()
+
+    print("\n\nWord So Far: "+to_display+"\nIncorrect Letter Picks: "+wrong_picks[1][1:])
+    if IS_CHEAT:
+        print("Current Word: "+to_guess)
+    current_pick=input("Input A Letter, "+str(PICK_LIMIT-wrong_picks[0])+" Picks Left: ").lower()[0]
 
     if current_pick not in all_picks:
         all_picks.append(current_pick)
     else:
-        print("Invalid Letter")
+        print("\nLetter "+current_pick+" Has Already Been Chosen")
         continue
 
     if current_pick in to_guess:
-        print("Letter "+current_pick.upper()+" Is In The Word")
+        print("\nLetter "+current_pick.upper()+" Is In The Word")
         for pos,letter in enumerate(to_guess):
             if letter in current_pick:
                 to_display=to_display[:pos]+current_pick+to_display[pos+1:]
     else:
-        wrong_picks+=1
-        print("Letter "+current_pick.upper()+" Is Not In The Word")
+        wrong_picks[0]+=1
+        print("\nLetter "+current_pick.upper()+" Is Not In The Word")
 
-    if to_display==to_guess:
+    if to_display == to_guess:
         print("\nYou Win! Word Was "+to_guess.capitalize())
         break
-    elif wrong_picks == PICK_LIMIT:
+    elif wrong_picks[0] == PICK_LIMIT:
         print("\nYou Lose! Word Was "+to_guess.capitalize())
         break
